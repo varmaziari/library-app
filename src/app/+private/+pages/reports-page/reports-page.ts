e8bcc0637e7ad622c0aa4ce72c47dccdc893aaf3
varmaziari <tinavarmaziari85@gmail.com>
2025-12-13 15:00:51 +0330

    pages is creation

---
+++ src/app/+private/+pages/reports-page/reports-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reports-page',
+  imports: [],
+  templateUrl: './reports-page.html',
+  styleUrl: './reports-page.css',
+})
+export class ReportsPage {
+
+}
