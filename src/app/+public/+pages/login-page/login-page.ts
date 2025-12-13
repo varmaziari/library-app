7ffbfd0e7696a01c10852ffc0edbbf8f4b0cfa56
varmaziari <tinavarmaziari85@gmail.com>
2025-12-13 16:14:31 +0330

    loginPage is complete

--- src/app/+public/+pages/login-page/login-page.ts
+++ src/app/+public/+pages/login-page/login-page.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
-
+  router=inject(Router)
+  loginform:LoginForm={
+    username:'',
+    password:'',
+    remember:false
+  };
+  message:string='';
+  check(){
+    if (this.loginform.username=='admin' && this.loginform.password=='admin') {
+      this.router.navigateByUrl('/private')
+    } 
+    else {
+      this.message='نام کاربری یا رمز عبور صحیح نمیباشد';
+    }
+  }
+}
+export interface LoginForm{
+  username:string;
+  password:string;
+  remember:boolean;
 }
